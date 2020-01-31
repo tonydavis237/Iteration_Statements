9eed54607fc57f51978c67bbc7dbd10830a00373
Tony Davis <anthony79@mail.usf.edu>
2020-01-31 13:15:20 -0500

    Commit all file changes

--- Program.cs
+++ Program.cs
@@ -21,10 +21,23 @@ namespace Iteration_Statements
                 // This variable will be used to run iterative statements
                 int value = int.Parse(input);
                 // for loop to display the users entered value and the current value in the loop
-                for (int count = 1; count <= value; count++)
+                if ((value >= 1) && (value <= 100))
                 {
-                    Console.WriteLine("You have entered " + value.ToString() + ". " + "This is the current integer value in the loop: " + count.ToString());
+                     for (int count = 1; count <= value; count++)
+                     {
+                         // Display entered user value and current integer value in the loop
+                         Console.WriteLine("You have entered " + value.ToString() + ". " + "This is the current integer value in the loop: " + count.ToString());
+                         
+                     }
                 }
+
+                else
+                {
+                            Console.WriteLine("You have entered an integer not in the restricted parameters");
+                            Console.WriteLine("Please enter an integer between the values of 1 and 100 and try again");
+                            Console.WriteLine("To try again, exit the program by pressing any key");
+                }
+
             } // End of try
             catch
             {
